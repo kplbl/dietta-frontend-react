d3a3d939e6b93b0ebb3d36840a1640078b085bff
kplbl <51096422+kplbl@users.noreply.github.com>
2021-12-22 19:15:52 +0100

    added register form

--- src/pages/user/RegisterForm.jsx
+++ src/pages/user/RegisterForm.jsx
@@ -1,3 +1,46 @@
-export default function RegisterForm() {
-  return <div></div>;
-}
+import { useContext, useState } from 'react';
+import UserContext from '../../context/user/UserContext';
+
+const RegisterForm = () => {
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+    email: '',
+  });
+  const userContext = useContext(UserContext);
+
+  const { register } = userContext;
+
+  const onChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    register(form.username, form.password, form.email);
+  };
+
+  return (
+    <div className="w-96">
+      <form onSubmit={onSubmit} className="bg-white rounded px-8 pt-6 pb-8 mb-4 space-y-6">
+        <div className="">
+          <label className="block text-sm text-gray-500">Uporabniško ime</label>
+          <input className="inputbox" type="text" name="username" onChange={onChange} />
+        </div>
+        <div>
+          <label className="block text-sm text-gray-500">Geslo</label>
+          <input className="inputbox" type="password" name="password" onChange={onChange} />
+        </div>
+        <div>
+          <label className="block text-sm text-gray-500">Email</label>
+          <input className="inputbox" type="email" name="email" onChange={onChange} />
+        </div>
+        <div className="flex justify-between">
+          <button className="btn btn-primary">Registriraj se</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
