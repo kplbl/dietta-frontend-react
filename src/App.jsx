2f1bf48e75443ac2afea15bd42ef8bd51a6f6e22
kplbl <51096422+kplbl@users.noreply.github.com>
2021-12-22 12:33:53 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react';
+import Navbar from './layout/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import Diary from './pages/diary/Diary';
+import Foods from './pages/foods/Foods';
+import Profile from './pages/profile/Profile';
+import LoginForm from './pages/user/LoginForm';
+import RegisterForm from './pages/user/RegisterForm';
+import { UserProvider } from './context/user/UserContext';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-cyan-700">bohdej</h1>
-    </div>
+    <UserProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/diary" element={<Diary />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/foods" element={<Foods />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </UserProvider>
   );
 }
 
