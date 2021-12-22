dfa385fe824463ef7d80bf1723c614b3ad52bab6
kplbl <51096422+kplbl@users.noreply.github.com>
2021-12-22 19:14:56 +0100

    moved user context provider and load user on mount

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,33 @@ import Foods from './pages/foods/Foods';
 import Profile from './pages/profile/Profile';
 import LoginForm from './pages/user/LoginForm';
 import RegisterForm from './pages/user/RegisterForm';
-import { UserProvider } from './context/user/UserContext';
+
+import { useEffect, useContext } from 'react';
+import UserContext from './context/user/UserContext';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+  const userContext = useContext(UserContext);
+  const { loadUser } = userContext;
+  const [token] = useLocalStorage('token', '');
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   return (
-    <UserProvider>
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/diary" element={<Diary />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/foods" element={<Foods />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterForm />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </UserProvider>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/foods" element={<Foods />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
