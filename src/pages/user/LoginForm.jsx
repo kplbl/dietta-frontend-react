ad7058d53a8bd4a8ff89e92a4005f0a9bc743760
kplbl <51096422+kplbl@users.noreply.github.com>
2021-12-22 19:16:01 +0100

    added login form

--- src/pages/user/LoginForm.jsx
+++ src/pages/user/LoginForm.jsx
@@ -1,3 +1,47 @@
-export default function LoginForm() {
-  return <div>Login Form</div>;
-}
+import { useContext } from 'react';
+import { useState } from 'react';
+import UserContext from '../../context/user/UserContext';
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+  });
+
+  const userContext = useContext(UserContext);
+
+  const { login } = userContext;
+
+  const onChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    login(form.username, form.password);
+  };
+
+  return (
+    <div className="w-96">
+      <form onSubmit={onSubmit} className="bg-white rounded px-8 pt-6 pb-8 mb-4 space-y-6">
+        <div className="">
+          <label className="block text-sm text-gray-500">Uporabniško ime</label>
+          <input className="inputbox" type="text" name="username" onChange={onChange} />
+        </div>
+        <div>
+          <label className="block text-sm text-gray-500">Geslo</label>
+          <input className="inputbox" type="password" name="password" onChange={onChange} />
+        </div>
+        <div className="flex justify-between">
+          <button className="btn btn-primary">Prijavi se</button>
+          <Link to="/register" className="btn btn-primary">
+            Registriraj se
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
