053a25601526ec3558f2cb48fdca6645a331ca7e
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-11 11:29:39 +0100

    debug, token

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -6,11 +6,7 @@ function Welcome() {
   return (
     <div>
       <div>token: {token}</div>
-      <div>
-        <button onClick={() => setToken('test1')} className="px-4 py-2 rounded-md bg-slate-500">
-          test
-        </button>
-      </div>
+      <div></div>
     </div>
   );
 }
