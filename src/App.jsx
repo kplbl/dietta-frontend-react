dcc7cd536b4efc0ef90d1c110377526df840ee88
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-15 14:30:49 +0100

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ import RegisterForm from './pages/user/RegisterForm';
 import { useEffect, useContext } from 'react';
 import UserContext from './context/user/UserContext';
 import useLocalStorage from './hooks/useLocalStorage';
+import PrivateRoute from './pages/PrivateRoute';
 
 function App() {
   const userContext = useContext(UserContext);
   const { loadUser } = userContext;
-  const [token] = useLocalStorage('token', '');
+
   useEffect(() => {
     loadUser();
   }, []);
@@ -24,9 +25,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Welcome />} />
-          <Route path="/diary" element={<Diary />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/foods" element={<Foods />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            {/* to je outlet v private routu */}
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/foods" element={<PrivateRoute />}>
+            <Route path="/foods" element={<Foods />} />
+          </Route>
+          <Route path="/diary" element={<PrivateRoute />}>
+            <Route path="/diary" element={<Diary />} />
+          </Route>
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
         </Routes>
